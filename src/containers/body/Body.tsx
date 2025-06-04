f3280d8318cf9696f39188bfd820d6de8c44dd4b
BatBeka <bekarichman@gmail.com>
2025-06-04 20:15:15 +0400

    Change hero section image, change document icon and title, add feature of navbar hiding on scroll

--- src/containers/body/Body.tsx
+++ src/containers/body/Body.tsx
@@ -1,5 +1,5 @@
-import PlaceholderImage from "../../assets/placeholder-image.png";
 import RetrowaveBeach from "../../assets/Retrowave-beach-and-palms.jpg";
+import ViceCityImage from "../../assets/hero-image-vice-city.png";
 import Carousel from "../../components/carousel/Carousel";
 
 export default function Body() {
@@ -13,7 +13,7 @@ export default function Body() {
 
 	return (
         <div className="w-full">
-            <img className="max-h-[400px] sm:max-h-[550px] md:max-h-[750px] w-full object-cover" src={PlaceholderImage} alt="placeholder" />
+            <img className="max-h-[400px] sm:max-h-[550px] md:max-h-[750px] w-full object-cover" src={ViceCityImage} alt="placeholder" />
             <h2 className="my-4 text-center text-primary-color text-2xl md:text-4xl">Your Text Here</h2>
             <Carousel srcCollection={carouselSrcCollection} name="" />
         </div>
--- src/containers/navbar/Navbar.tsx
+++ src/containers/navbar/Navbar.tsx
@@ -1,32 +1,70 @@
-import { useState } from "react";
-import Logo from "../../assets/Sunsetwave-Logo.png";
-import { VscMenu } from "react-icons/vsc";
+import { useEffect, useState } from "react";
+import LogoVice from "../../assets/logo-vice.png";
+import { LuTreePalm } from "react-icons/lu";
 import OffcanvasMenu from "../offcanvasMenu/OffcanvasMenu";
+import { throttle } from "lodash";
+
+const SCROLL_THROTTLE_MS = 200;
 
 export default function Navbar() {
 	const [toggleMenu, setToggleMenu] = useState(false);
+	const [toggleNavbar, setToggleNavbar] = useState(true);
+
+	useEffect(() => {
+		let navbarToggleHandler = (() => {
+			let prevPageYOffset = 0;
+
+			return () => {
+				if (window.pageYOffset > prevPageYOffset)
+					setToggleNavbar(false);
+				else setToggleNavbar(true);
+				prevPageYOffset = window.pageYOffset;
+			};
+		})();
+
+		navbarToggleHandler = throttle(navbarToggleHandler, SCROLL_THROTTLE_MS);
+
+		window.addEventListener("scroll", navbarToggleHandler);
+
+		return () => {
+			window.removeEventListener("scroll", navbarToggleHandler);
+		};
+	}, []);
 
 	return (
 		<>
 			<nav
-				className="fixed top-0 left-0 w-full z-50 
-    bg-white/40 backdrop-blur-md border-b border-white/20 
+				className={`fixed ${
+					toggleNavbar
+						? "top-0 pointer-events-auto opacity-100"
+						: "-top-full pointer-events-none opacity-0"
+				} transition-all left-0 duration-300 w-full z-50 
+    bg-white/10 backdrop-blur-md border-b border-white/20 
     shadow-[0_4px_30px_rgba(0,0,0,0.1)] 
     h-[40px] sm:h-[50px] md:h-[60px] 
-    flex items-center justify-between ps-2 pe-4"
+    flex items-center justify-between ps-2 pe-4`}
 			>
-				<div className="h-full">
+				<div className="h-full p-3">
 					<img
 						className="cursor-pointer h-full max-w-[150px] sm:max-w-[300px] md:max-w-[400px] w-full"
-						src={Logo}
+						src={LogoVice}
 						alt="Retrowave logo"
 					/>
 				</div>
 				<div className="h-full w-[25px]">
-					<VscMenu onClick={() => setToggleMenu(true)} className="cursor-pointer text-primary-color w-full h-full" />
+					<button
+						className="w-full h-full"
+						type="button"
+						onClick={() => setToggleMenu(true)}
+					>
+						<LuTreePalm className="cursor-pointer text-primary-color w-full h-full" />
+					</button>
 				</div>
 			</nav>
-			<OffcanvasMenu toggleMenu={toggleMenu} setToggleMenu={setToggleMenu} />
+			<OffcanvasMenu
+				toggleMenu={toggleMenu}
+				setToggleMenu={setToggleMenu}
+			/>
 		</>
 	);
 }
